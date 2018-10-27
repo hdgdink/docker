9962a797554ea1197ca6e5e8ba096902d0672c1e
hdgdink <hdgdink@list.ru>
2018-10-27 23:20:51 +0600

    fix docker-compose

--- mySQL/scr/init.sh
+++ mySQL/scr/init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'changeme' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+/usr/bin/mysqld_safe &
+	sleep 10s
+
+	echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'changeme' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
+	killall mysqld
+	sleep 10s
+
+
+/usr/bin/mysqld_safe
